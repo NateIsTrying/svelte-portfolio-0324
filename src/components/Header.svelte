388f2d21b5d45ab569cdb338c51aea6f607f534e
Nathan Kim <novicenate@pop-os.localdomain>
2024-03-06 21:56:44 -0500

    added tons more content into Main.svelte

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -8,15 +8,14 @@
     ]
 </script>
 
-<header class={'sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid '+ (
-    y > 0 
-    ? ' py-4 bg-slate-950 border-violet-950' 
+<header class={'sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border-b border-solid '+ (
+    y > 0 ? ' py-4 bg-slate-950 border-violet-950'
     : ' py-6 bg-transparent border-transparent')}
 >
     <h1 class="font-medium">
         <b class="font-bold poppins">Nathan</b> Kim
     </h1>
-    <div class="sm:flex items-center">
+    <div class="sm:flex ml-auto pr-4 items-center gap-4 hidden">
         {#each tabs as tab, index}
             <a 
                 href={tab.link} 
